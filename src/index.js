33af37cc9afb1cb9b2d8385f145cd2bb4d5187e7
Nilesh Patil <nil041297@gmail.com>
2023-04-08 19:41:29 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -8,20 +8,39 @@ import { movies, search } from "./reducers";
 
 const rootReducer = combineReducers({
   movies,
-  search
-})
+  search,
+});
 
-const store = configureStore({reducer: rootReducer});
-console.log('Before store.getState: ', store.getState());  
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       //middleware code
+//       console.log("ACTION TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    //middleware code
+    console.log("ACTION TYPE = ", action.type);
+    next(action);
+  };
+
+const store = configureStore({ reducer: rootReducer, middleware: [logger] });
+console.log("Before store.getState: ", store.getState());
 // store.dispatch({
 //   type: 'ADD_MOVIES',
 //   movies:[{name: 'Superman'}]
-// }); 
-// console.log('After store.getState: ', store.getState()); 
+// });
+// console.log('After store.getState: ', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App store={store}/>
+    <App store={store} />
   </React.StrictMode>
 );
